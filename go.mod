6bdf5c4464009af301f654e38cea8b745e80b2e4
Roberto <robertocgural@gmail.com>
2024-02-29 09:16:18 -0300

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/robertocgural/godesde0
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/robertocgural/godesde0/variables"
+
+func main() {
+	variables.MostroEnteros()
+}
